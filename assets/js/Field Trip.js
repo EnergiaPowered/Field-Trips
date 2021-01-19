7a44d48cfeaf48465eb9b44366349fb79d772a61
Waleed Hesham <waleedhesham2001@gmail.com>
2021-01-19 22:42:06 +0200

    Created The Validation Function

---
+++ assets/js/Field Trip.js
@@ -0,0 +1,69 @@
+let name = document.getElementById('name'),
+	email = document.getElementById('email'),
+	phone = document.getElementById('phone'),
+	facebook = document.getElementById('Facebook'),
+	university = document.getElementById('university'),
+	college = document.getElementById('college'),
+	department = document.getElementById('department'),
+	academic_year = document.getElementById('academic_year'),
+	last_GPA = document.getElementById('last_GPA'),
+	trips = document.getElementById('trips');
+
+let error_msg = document.getElementsByClassName('text-danger');
+
+let submit = document.getElementById('submit');
+
+
+function validate(){
+	let stat = true;
+	if(name.value == ""){
+		error_msg[0].style.display = "block";
+		stat = false;
+	}else{
+		error_msg[0].style.display = "none";
+	}
+
+	if(email.value == "" || !email.value.includes('@')){
+		error_msg[1].style.display = "block";
+		stat = false;
+	}else{
+		error_msg[1].style.display = "none";
+	}
+
+	if(phone.value == "" || isNaN(phone.value) || phone.value.length != 11){
+		error_msg[2].style.display = "block";
+		stat = false
+	}else{
+		error_msg[2].style.display = "none";
+	}
+
+	if(facebook.value == "" || !facebook.value.includes('https://')){
+		error_msg[3].style.display = "block";
+		stat = false;
+	}else{
+		error_msg[3].style.display = "none";
+	}
+
+	if(academic_year.value == "" || isNaN(academic_year.value) || academic_year.value > 7 || academic_year.value < 0){
+		error_msg[4].style.display = "block";
+		stat = false;
+	}else{
+		error_msg[4].style.display = "none";
+	}
+
+	if(last_GPA.value == "" || isNaN(last_GPA.value) || last_GPA.value > 5 || last_GPA.value < 0){
+		error_msg[5].style.display = "block";
+		stat = false;
+	}else{
+		error_msg[5].style.display = "none";
+	}
+
+	return stat;
+}
+
+submit.addEventListener("click", function(e){
+	e.preventDefault();
+	if(!validate()){
+		console.log('error');
+	}
+});
